282c26420735192069f40bbf39ec2e8bd8de3f55
Iflaq khurshid <112414810+Iflaqbhat@users.noreply.github.com>
2024-10-17 10:27:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 function firstWord(s) {
   // your code here
+	let ans="";
+	if(s==''){
+		return '';
+	}
+	for(let i=0;i<s.length;i++){
+		if(s[i]!==''){
+			ans+=s[i];
+		}
+	}
+	return ans;
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
